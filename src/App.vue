5fd396f432d56dd5103d7fea34088206ebec4979
DColacilli <davidcolacilli@live.com>
2023-01-13 12:50:58 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
+  <div>
+    <header>
+      <h1>david colacilli</h1>
+      <nav class="main-nav">
+        <router-link to="/">About</router-link>
+        <router-link to="/work">Work</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </nav>
+    </header>
     <div class="container">
-      <header>
-        <!-- <router-link to="/"> -->
-          <!-- <img class="profile" src="profile-picture.jpeg" alt="Profile Picture"> -->
-          <h1>david colacilli</h1>
-          <!-- <div>
-            <p>UX designer / UI Developer</p>
-          </div> -->
-        <!-- </router-link> -->
-        <nav class="main-nav">
-          <router-link to="/">About</router-link>
-          <router-link to="/work">Work</router-link>
-          <a href="#contact">Contact</a>
-        </nav>
-      </header>
       <router-view></router-view>
       <Footer/>
     </div>
+  </div>
 </template>
 <script>
 // @ is an alias to /src
@@ -36,13 +32,6 @@ body {
   font-family: Lora, Georgia, Cambria, "Times New Roman", Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-    /* background-color: #00589B; */
-    /* background-color: #00A0B0; */
-    /* background-color: #CF5C78; */
-    /* background-color: #F5DF4D */
-    /* background-color: #F0EEE9; */
-    /* background-color: #939597; */
-    /* background-color: #FFF; */
 }
 
 h1,h2,h3,h4,h5 {
@@ -56,86 +45,32 @@ h1 {
   font-weight: bold;
   margin: 1em 0 0.5em;
   text-align: center;
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
-/*h1 + p {
-  margin-bottom: 0.3rem;
-  margin-top: -.25rem;
-  font-family: Poppins, Helvetica, Arial, sans-serif;
-  font-size: .5rem;
-  text-transform: uppercase;
-  letter-spacing: 1.5px;
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; 
-}*/
 h2 {
   font-family: Poppins, Helvetica, Arial, sans-serif;
   font-weight: 700;
   font-size: 2em;
   line-height: 1.5;
   color: #DDD;
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
 h3 {
   margin: 0;
   padding: 0;
-  /* font-family: Poppins, Helvetica, Arial, sans-serif; */
-  /* font-weight: 700; */
   font-size: 1.5em;
   line-height: 125%;
   letter-spacing: normal;
   text-align: left;
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
 h4 {
-  /* font-family: Poppins, Helvetica, Arial, sans-serif; */
-  /* font-weight: 700; */
   font-family: Lora, Georgia, Cambria, "Times New Roman", Times, serif;
   font-style: italic;
   line-height: 1.5;
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
 h5 {
   font-family: Poppins, Helvetica, Arial, sans-serif;
   font-weight: 700;
   margin-bottom: 0.75em;
   line-height: 1.5;
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
 p {
   margin: 10px 0;
@@ -144,22 +79,9 @@ p {
   line-height: 150%;
   text-align: left;
   word-break: break-word;
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
 a {
-    /* color: #00589B; */
-    /* color: #00A0B0; */
     color: #CF5C78;
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
 li {
   margin: .5em;
@@ -171,7 +93,6 @@ li {
   margin: 0 auto;
 }
 header {
-  /* background: #fafafa; */
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -180,13 +101,6 @@ header {
 .content-wrapper, footer {
   padding: 0 1em;
 }
-/* .profile {
-  border-radius: 50%;
-  width: 50px;
-  height: auto;
-  display: block;
-  margin-right: 1em;
-} */
 nav {
   font-family: Poppins, Helvetica, Arial, sans-serif;
   line-height: 1.5;
@@ -195,53 +109,19 @@ nav a {
   text-decoration: none;
 }
 .main-nav {
-  /* margin: 0 -1em 2em -1em; */
-  /* background: #fafafa; */
-  /* border-radius: 15px; */
-  /* padding: 0 5px; */
   display: flex;
   justify-content: center;
-
 }
 .main-nav a {
   display: inline-block;
   margin: 1em;
   opacity: .75
-
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
 .main-nav a.router-link-exact-active {
-  /* border-bottom: 2px solid rgba(0, 0, 0, .6); */
   font-weight: 700;
   opacity: 1
-
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
-}
-
-.additional-info {
-    /* color: #00589B; */
-    /* color: #00A0B0; */
-    /* color: #CF5C78; */
-    /* color: #F5DF4D */
-    /* color: #F0EEE9; */
-    /* color: #939597; */
-    /* color: #FFF; */
 }
 .additional-info p, .additional-info ul {
-  /* font-family: Lora, Georgia,Times,'Times New Roman',serif; */
-  /*font-family: 'DM Serif Text';*/
   font-family: Poppins, Helvetica, Arial, sans-serif;
   font-weight: 500;
   font-size: 13px;
@@ -249,17 +129,16 @@ nav a {
 
 @media (min-width: 590px) {
   header {
-    margin: 2em 1em 3em;
-    padding: 1em 1em 1em 1.5em;
-    background: #FFF;
+    margin: 1em 1em 3em;
+    padding: 0.5em 1em 0.5em 1.5em;
     border-radius: 10px;
     box-shadow: 0px 2px 6px 0px rgb(0 0 0 / 5%);
+    box-shadow: none;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
   }
   .main-nav {
-    /* background: #FCFCFC; */
     border-radius: 10px;
   }
   h1 {
@@ -269,28 +148,5 @@ nav a {
     margin: 0;
     line-height: 1.5;
   }
-  /*
-  .profile {
-    width: 90px;
-  }
-  h1 {
-    font-size: 2.5rem;
-  }
-  h1 + p {
-    margin-bottom: 0.3rem;
-    font-size: .9rem;
-  }
-  */
-  /*
-  html {
-    background-attachment: fixed;
-    background-blend-mode: overlay;
-    background-color: #f8f8f8;
-    background-image: url('https://images.unsplash.com/uploads/141103282695035fa1380/95cdfeef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1948&q=80');
-    background-position: center top;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-  */
 }
 </style>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,114 +1,159 @@
 <template>
-  <div class=content-wrapper>
-    <article class="content">
-      <h2>UX Design</h2>
-      <section>
-          <h3>Presta</h3>
-          <figure>
-            <a href="https://lnkd.in/dZwMz9i" target="_blank">
-              <img src="presta-low.png" alt="Presta App slide">
-              <caption>See presentation</caption>
-            </a>
-          </figure>
-          <h4>App Design. Project for Coderhouse UX/UI Design course (February 2021 - May 2021)</h4>
-          <p>The designed application is a tool to register borrowed and lent items, as well as configuring reminders for the same purpose.</p>
-          <p>The full UX journey was explored.</p>
-          <div class="additional-info">
-            <h5>Tasks</h5>
-            <ul>
-                <li>Empathise with the user's needs and goals</li>
-                <li>Research and analyse; build user personas</li>
-                <li>Design: Wireframing</li>
-                <li>Testing and iteration</li>
-                <li>Design: Functional prototype</li>
-                <li>Logo design</li>
-                <li>IU Kit, moodboard</li>
-                <li>Mockups</li>
-            </ul>
-            <h5>Tools</h5>
-          <p>Figma, Optimal Workshop, Whimsical</p>
-          </div>
-      </section>
-      <section>
-          <h3>Benchmarking Tool</h3>
-          <h4>UX Designer in a project involving redesign of a benchmarking web application (January 2019 - June 2020)</h4>
-          <p>The client offers many products/services in the benchmarking industry, which is the automation of the process of data gathering resulting in significant metrics. The main purpose is to identify solutions to close performance gaps and jumpstart improvement in different industries. </p>
-          <p>The application serves as a great tool not only to evaluate internal metrics but also to compare performance between different companies in the same industry. </p>
-          <div class="additional-info">
-            <h5>Tasks</h5>
-            <ul>
-                <li>Understand and analyze the business’ goals and requirements</li>
-                <li>Detect issues in current app </li>
-                <li>Provide design solutions </li>
-                <li>Test and discuss with the Dev Team and Product Owner </li>
-                <li>Provide support to the Dev Team during the implementation of each section </li>
-                <li>Work with Agile methodology (Scrum)</li>
-            </ul>
-            <h5>Tools</h5>
-          <p>Figma, InVision, Whimsical.</p>
-          </div>
-      </section>
-      <section>
-          <h3>Librarian's App</h3>
-          <h4>UX Designer and frontend developer in a project involving redesign and development of a long list management tool (August 2017 - December 2018)</h4>
-          <p>The Client is the engine behind a network of important organizations and initiatives. It aims to improve our planet through catalytic philanthropy, inspirational experiences, and scientific and technological breakthroughs.</p>
-          <p>The project consisted of the redesign of an internal tool for the creation of playlists and the management of big amounts of recordings.</p>
-          <div class="additional-info">
-            <h5>Tasks</h5>
-              <p>UI, UX, Frontend tasks, including:</p>
+  <div class="section-wrapper">
+    <aside>
+      <h5>UX Design</h5>
+      <ul>
+        <li>
+          <a href="#project-the-hackett-group">The Hackett Group</a>
+        </li>
+        <li>
+          <a href="#project-presta">Presta</a>
+        </li>
+        <li>
+          <a href="#project-vulcan">Vulcan</a>
+        </li>
+      </ul>
+      <h5>Web UI Development</h5>
+      <ul>
+        <li>
+          <a href="#project-intouch">Intouch Solutions</a>
+        </li>
+        <li>
+          <a href="#project-chicago-tribune">Chicago Tribune</a>
+        </li>
+      </ul>
+    </aside>
+    <div class="content-wrapper">
+      <article class="content">
+        <h2>UX Design</h2>
+        <section id="project-the-hackett-group">
+            <h3>The Hackett Group</h3>
+            <figure>
+              <img src="screenshot-thg.png" alt="The Hackett Group App">
+            </figure>
+            <h4>UX Designer at Endava for The Hackett Group, working on the redesign and maintenance of Quantum Leap, a benchmarking web application (January 2019 - today)</h4>
+            <p>The client offers many products/services in the benchmarking industry, which is the automation of the process of data gathering resulting in significant metrics. The main purpose is to identify solutions to close performance gaps and jumpstart improvement in different industries. </p>
+            <p>The application serves as a great tool not only to evaluate internal metrics but also to compare performance between different companies in the same industry. </p>
+            <div class="additional-info">
+              <h5>Tasks</h5>
               <ul>
-                  <li>UX for complex/uncommon components</li>
-                  <li>Building new components in Angular</li>
-                  <li>Styling (Less CSS)</li>
-                  <li>Bug fixing</li>
-                  <li>solve reported bugs</li>
-                  <li>participate in estimation process</li>
-                  <li>participate in demos for stakeholders</li>
-                  <li>working with Agile methodology (Scrum)</li>
+                  <li>Understand and analyze the business’ goals and requirements</li>
+                  <li>Detect issues in current app </li>
+                  <li>Provide design solutions </li>
+                  <li>Test and discuss with the Dev Team and Product Owner </li>
+                  <li>Provide support to the Dev Team during the implementation of each section </li>
+                  <li>Work with Agile methodology (Scrum)</li>
               </ul>
+              <h5>Tools</h5>
+            <p>Figma, InVision, Whimsical.</p>
+            </div>
+            <a href="#" class="back-to-top">Back to Top</a>
+        </section>
+        <section id="project-presta">
+            <h3>Presta</h3>
+            <figure>
+              <a href="https://lnkd.in/dZwMz9i" target="_blank">
+                <img src="presta-low.png" alt="Presta App slide">
+              </a>
+            </figure>
+            <h4>App Design. Project for Coderhouse UX/UI Design course (February 2021 - May 2021)</h4>
+            <p>The designed application is a tool to register borrowed and lent items, as well as configuring reminders for the same purpose.</p>
+            <p>The full UX journey was explored.</p>
+            <div class="additional-info">
+              <h5>Tasks</h5>
+              <ul>
+                  <li>Empathise with the user's needs and goals</li>
+                  <li>Research and analyse; build user personas</li>
+                  <li>Design: Wireframing</li>
+                  <li>Testing and iteration</li>
+                  <li>Design: Functional prototype</li>
+                  <li>Logo design</li>
+                  <li>IU Kit, moodboard</li>
+                  <li>Mockups</li>
+              </ul>
+              <h5>Tools</h5>
+            <p>Figma, Optimal Workshop, Whimsical</p>
+            </div>
+            <br>
+            <a href="https://lnkd.in/dZwMz9i" target="_blank">
+              See Presentation
+            </a>
+            <br><br><br>
+            <a href="#" class="back-to-top">Back to Top</a>
+        </section>
+        <section id="project-vulcan">
+            <h3>Vulcan</h3>
+            <h4>UX Designer and frontend developer at Endava for Vulcan Inc., working in a project involving redesign and development of a long list management tool (August 2017 - December 2018)</h4>
+            <p>The Client is the engine behind a network of important organizations and initiatives. It aims to improve our planet through catalytic philanthropy, inspirational experiences, and scientific and technological breakthroughs.</p>
+            <p>The project consisted of the redesign of an internal tool for the creation of playlists and the management of big amounts of recordings.</p>
+            <div class="additional-info">
+              <h5>Tasks</h5>
+                <p>UI, UX, Frontend tasks, including:</p>
+                <ul>
+                    <li>UX for complex/uncommon components</li>
+                    <li>Building new components in Angular</li>
+                    <li>Styling (Less CSS)</li>
+                    <li>Bug fixing</li>
+                    <li>solve reported bugs</li>
+                    <li>participate in estimation process</li>
+                    <li>participate in demos for stakeholders</li>
+                    <li>working with Agile methodology (Scrum)</li>
+                </ul>
+              <h5>Tools</h5>
+              <p>Angular, HTML, CSS, Moqups.</p>
+            </div>
+            <a href="#" class="back-to-top">Back to Top</a>
+        </section>
+      </article>
+      <article class="content">
+        <h2>Web UI Development</h2>
+        <section id="project-intouch">
+          <h3>Intouch Solutions</h3>
+          <h4>Web UI Developer at Endava for Intouch Solutions, working in several projects that involved team management and Web UI development for various applications and websites (November 2015 – July 2017)</h4>
+          <p>The client is a pharmaceutical marketing agency, with 15+ years of pharma experience, a variety of integrated products and services, industry expertise and leadership, and a "start digital" philosophy.</p>
+          <div class="additional-info">
+            <h5>Tasks</h5>
+            <ul>
+              <li>Web UI development</li>
+              <li>Direct communication with diverse Product Owners within the client’s company</li>
+              <li>Coordination of multiple tasks amog a small team</li>
+              <li>Providing support to the Dev Team</li>
+              <li>Technical interviews for new hires</li>
+            </ul>
             <h5>Tools</h5>
-            <p>Angular, HTML, CSS, Moqups.</p>
+            <p>HTML, CSS, jQuery, Javascript, Visual Studio, TFS, Adobe Photoshop, Sketch</p>
           </div>
-      </section>
-    </article>
-    <article class="content">
-      <h2>Web UI Development</h2>
-      <section>
-        <h3>iOS WebApps</h3>
-        <h4>UI Developer in a project involving team management and web ui development for various applications and websites (November 2015 – July 2017)</h4>
-        <p>The client is a pharmaceutical marketing agency, with 15+ years of pharma experience, a variety of integrated products and services, industry expertise and leadership, and a "start digital" philosophy.</p>
-        <div class="additional-info">
-          <h5>Tasks</h5>
-          <ul>
-            <li>Web UI development</li>
-            <li>Direct communication with diverse Product Owners within the client’s company</li>
-            <li>Coordination of multiple tasks amog a small team</li>
-            <li>Providing support to the Dev Team</li>
-            <li>Technical interviews for new hires</li>
-          </ul>
-          <h5>Tools</h5>
-          <p>HTML, CSS, jQuery, Javascript, Visual Studio, TFS, Adobe Photoshop, Sketch</p>
-        </div>
-      </section>
-      <section>
-        <h3>Online News</h3>
-        <h4>UI Developer in a project involving small team management and Web UI development for a multi-client website (July 2014 - November 2015)</h4>
-        <p>The client is a very well-known American newspaper print and online media publishing company in LA, Chicago and many satellite cities.</p>
-        <div class="additional-info">
-          <h5>Tasks</h5>
-          <ul>
-            <li>Web UI development</li>
-            <li>Team management</li>
-            <li>Technical interviews for new hires</li>
-          </ul>
-          <h5>Tools</h5>
-          <p>HTML, CSS, jQuery, Adobe Photoshop</p>
-        </div>
-      </section>
-    </article>
+          <a href="#" class="back-to-top">Back to Top</a>
+        </section>
+        <section id="project-chicago-tribune">
+          <h3>Chicago Tribune</h3>
+          <h4>Web UI Developer at Globan for Chicago Tribune, working in the redesign of the Company's online newspapers; in charge of a small team management and doing Web UI development for their multi-client website platform (July 2014 - November 2015)</h4>
+          <p>The client is a very well-known American newspaper print and online media publishing company in LA, Chicago and many satellite cities.</p>
+          <div class="additional-info">
+            <h5>Tasks</h5>
+            <ul>
+              <li>Web UI development</li>
+              <li>Team management</li>
+              <li>Technical interviews for new hires</li>
+            </ul>
+            <h5>Tools</h5>
+            <p>HTML, CSS, jQuery, Adobe Photoshop</p>
+          </div>
+          <a href="#" class="back-to-top">Back to Top</a>
+        </section>
+      </article>
+    </div>
   </div>
 </template>
 <style scoped>
+.section-wrapper {
+  position: relative;
+}
+aside {
+  position: absolute;
+  left: -240px;
+  top: 0;
+}
 article {
   margin: 2em 0 5em;
 }
@@ -137,5 +182,13 @@ figure img {
   width: 100%;
   border-radius: 20px;
   box-shadow: 0px 1px 3px 0 rgb(0 0 0 / 15%);
+}
+.back-to-top {
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: .6em;
+  display: block;
+  text-align: right;
+  /* font-weight: 600; */
+  /* text-decoration: none; */
 }
 </style>
